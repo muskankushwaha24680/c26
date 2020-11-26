f8d3f23df6d1d0fd6c9134058ed6e7ae34a8322b
muskankushwaha24680 <kushwahapreeti2003@gmail.com>
2020-11-26 13:46:20 +0530

    platform added

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
-
+platform = new Ground(150,305,300,170);
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -39,6 +39,7 @@ function setup(){
 
 function draw(){
     background(backgroundImg);
+    
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
@@ -59,4 +60,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
